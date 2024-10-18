d7c9344f13d7628e7d76e662a3bcf1130b272a4e
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-10-19 00:18:00 +0530

    created

--- lib/home/Subtraction.dart
+++ lib/home/Subtraction.dart
@@ -5,6 +5,62 @@ class Subtraction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Subtraction"),
+          backgroundColor: Colors.brown,
+        ),
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              children: [
+                Text("Enter a Number:"),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Number"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Enter a Number:"),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Number"),
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 30,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orangeAccent),
+                    onPressed: () {},
+                    child: Text("Subtract")
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 30,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orangeAccent),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Home")
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
